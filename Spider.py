db1e75b05e5c1f4ce1c169a1c5fdb9bb50e69d86
chaos <497386856@qq.com>
2016-05-01 13:29:32 +0800

    完成英雄数据的打印函数

--- Spider.py
+++ Spider.py
@@ -7,7 +7,7 @@ class LolSpider:
     #========attr=============
     ServerName = ""
     UserId=""
-    data=json
+    data=list() #list_of_dict or list_of_str
 
     User_info_url = "http://API.xunjob.cn/playerinfo.php?" #玩家信息
     Record_url="http://API.xunjob.cn/Record.php?" #战绩信息
@@ -29,6 +29,7 @@ class LolSpider:
             requestsRes = requests.post(url=TheUrl) #发起请求
             RowData = json.loads(requestsRes.text) #处理json
             content= RowData['content'] #content为字典组成的list
+            self.data = content
             return content
             pass
 
@@ -48,6 +49,29 @@ class LolSpider:
             #隐藏分信息
             pass
 
+    def SettleHerosData(self):
+        #用于将list_of_dict整理为list_of_str
+        #print(self.data)
+
+        data_str_list =list()
+        dict_keys = self.data[0].keys()
+
+        for  i in self.data:
+            #print(i)
+            tempStr = "英雄名字:"+i['championNameCN']
+            tempStr += "  MVP次数:"+str(i['totalMVP'])
+            tempStr += "  场均[杀/死/助]:"+str(i['averageKDA'])
+            tempStr += "  战损比:"+str(i['averageKDARating'])
+            tempStr += "  胜率:"+str(i['winRate'])
+            tempStr += "  使用场次:"+str(i['matchStat'])
+            tempStr += "  场均输出"+str(i['averageDamage'])
+            tempStr += "  场均分钟经济:"+str(i['averageEarn'])
+            tempStr += "  场均补兵"+str(i['averageMinionsKilled'])
+            data_str_list.append(tempStr)
+
+        print(data_str_list)
+        return data_str_list
+
 
     def SaveData(self):
         pass
@@ -57,3 +81,4 @@ class LolSpider:
 if __name__=="__main__":
     one = LolSpider("祖安","爸爸在等你")
     one.GetData('h')
+    one.SettleHerosData()
